ee4a6c0965daf2cfe4db058667a6573c36253b71
Ming <39115691+cro4737@users.noreply.github.com>
2020-07-01 22:39:41 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,11 @@
-<?php phpinfo(); ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+   <?php phpinfo(); ?>
+  </body>
+</html>
+
